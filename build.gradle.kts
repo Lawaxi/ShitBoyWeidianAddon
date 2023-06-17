90e2154f15e72929e071c1b381eb593411f52f0e
Lawaxi <2330234142@qq.com>
2023-06-17 20:15:13 +0800

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "net.lawaxi"
-version = "0.1.0-test4"
+version = "0.1.0-test5"
 
 repositories {
     maven("https://maven.aliyun.com/repository/public")
--- src/main/java/net/lawaxi/sbwa/ShitBoyWeidianAddon.java
+++ src/main/java/net/lawaxi/sbwa/ShitBoyWeidianAddon.java
@@ -18,7 +18,7 @@ public final class ShitBoyWeidianAddon extends JavaPlugin {
     public static WeidianHandler weidianHandler;
 
     private ShitBoyWeidianAddon() {
-        super(new JvmPluginDescriptionBuilder("net.lawaxi.shitboyWA", "0.1.0-test4")
+        super(new JvmPluginDescriptionBuilder("net.lawaxi.shitboyWA", "0.1.0-test5")
                 .name("ShitBoyWeidianAddon")
                 .author("delay")
                 .dependsOn("net.lawaxi.shitboy", false)
